39d18517f700e6a2e6ce9de0ea093ee3bab485e8
Florian Westphal <fw@strlen.de>
2026-03-09 09:46:38 +0100

    tests: cli-tests.sh: add all, all-ns options
    1. Add "all", "all-ns" options to run all tests in a loop
    2. Expose a script return value for CI pipelines
    
    Expectation tests do not work for unknown reason, gets -EOPNOTSUPP from fedora kernel.  To be investigated.  Therefore excluded for the time being.
    
    Signed-off-by: Florian Westphal <fw@strlen.de>

--- tests/conntrack/cli-test.sh
+++ tests/conntrack/cli-test.sh
@@ -1,18 +1,25 @@
-CONNTRACK=conntrack
+#!/bin/bash
+
+CONNTRACK=../../src/conntrack
 
 SRC=1.1.1.1
 DST=2.2.2.2
 SPORT=2005
 DPORT=21
 
+ret=0
+lret=0
+
 case $1 in
 	dump)
 		echo "Dumping conntrack table"
 		$CONNTRACK -L
+		ret=$?
 		;;
 	flush)
 		echo "Flushing conntrack table"
 		$CONNTRACK -F
+		ret=$?
 		;;
 	new)
 		echo "creating a new conntrack"
@@ -21,38 +28,44 @@ case $1 in
 		 --orig-port-src $SPORT  --orig-port-dst $DPORT \
 		 --reply-port-src $DPORT --reply-port-dst $SPORT \
 		--state LISTEN -u SEEN_REPLY -t 50
+		ret=$?
 		;;
 	new-simple)
 		echo "creating a new conntrack (simplified)"
 		$CONNTRACK -I -s $SRC -d $DST \
 		-p tcp --sport $SPORT  --dport $DPORT \
 		--state LISTEN -u SEEN_REPLY -t 50
+		ret=$?
 		;;
 	new-nat)
 		echo "creating a new conntrack (NAT)"
 		$CONNTRACK -I -s $SRC -d $DST \
 		-p tcp --sport $SPORT  --dport $DPORT \
 		--state LISTEN -u SEEN_REPLY -t 50 --dst-nat 8.8.8.8
+		ret=$?
 		;;
 	get)
 		echo "getting a conntrack"
 		$CONNTRACK -G -s $SRC -d $DST \
 		-p tcp --sport $SPORT --dport $DPORT
+		ret=$?
 		;;
 	change)
 		echo "change a conntrack"
 		$CONNTRACK -U -s $SRC -d $DST \
 		-p tcp --sport $SPORT --dport $DPORT \
 		--state TIME_WAIT -u ASSURED,SEEN_REPLY -t 500
+		ret=$?
 		;;
 	delete)
 		$CONNTRACK -D -s $SRC -d $DST \
 		-p tcp --sport $SPORT --dport $DPORT
+		ret=$?
 		;;
 	output)
-		proc=$(cat /proc/net/ip_conntrack | wc -l)
+		proc=$(cat /proc/net/nf_conntrack | wc -l)
 		netl=$($CONNTRACK -L | wc -l)
-		count=$(cat /proc/sys/net/ipv4/netfilter/ip_conntrack_count)
+		count=$(cat /proc/sys/net/netfilter/nf_conntrack_count)
 		if [ $proc -ne $netl ]; then
 			echo "proc is $proc and netl is $netl and count is $count"
 		else
@@ -62,30 +75,56 @@ case $1 in
 				echo "now $proc"
 			fi
 		fi
+		ret=$?
 		;;
 	dump-expect)
 		$CONNTRACK -L expect
+		ret=$?
 		;;
 	flush-expect)
 		$CONNTRACK -F expect
+		ret=$?
 		;;
 	create-expect)
-		# requires modprobe ip_conntrack_ftp
+		conntrack -L
+		# modprobe nf_conntrack_ftp
 		$CONNTRACK -I expect --orig-src $SRC --orig-dst $DST \
 		--tuple-src 4.4.4.4 --tuple-dst 5.5.5.5 \
 		--mask-src 255.255.255.0 --mask-dst 255.255.255.255 \
 		-p tcp --orig-port-src $SPORT --orig-port-dst $DPORT \
 		-t 200 --tuple-port-src 10240 --tuple-port-dst 10241\
 		--mask-port-src 10 --mask-port-dst 300
+		ret=$?
 		;;
 	get-expect)
 		$CONNTRACK -G expect --orig-src 4.4.4.4 --orig-dst 5.5.5.5 \
-		--p tcp --orig-port-src 10240 --orig-port-dst 10241
+		--p tcp --orig-port-src 10240 --orig-port-dst 10241 \
+		--reply-port-src $DPORT --reply-port-dst $SPORT
+		ret=$?
 		;;
 	delete-expect)
 		$CONNTRACK -D expect --orig-src 4.4.4.4 \
 		--orig-dst 5.5.5.5 -p tcp --orig-port-src 10240 \
-		--orig-port-dst 10241
+		--orig-port-dst 10241 \
+		--reply-port-src $DPORT --reply-port-dst $SPORT
+		ret=$?
+		;;
+	all-ns)
+		unshare -n "$0" all
+		ret=$?
+		;;
+	all)
+		for T in new delete new-simple flush new-nat \
+			dump \
+			change output \
+			flush ; do
+			echo "Checking: $T"
+			"$0" "$T"
+			lret=$?
+
+			[ "$lret" -ne 0 ] && echo "FAIL: $T"
+			[ "$ret" -eq 0 ] && ret=$lret
+		done
 		;;
 	*)
 		echo "Usage: $0 [dump"
@@ -102,5 +141,9 @@ case $1 in
 		echo "		|create-expect"
 		echo "		|get-expect"
 		echo "		|delete-expect]"
+		echo "		|all-ns"
+		echo "		|all"
 		;;
 esac
+
+exit $ret
