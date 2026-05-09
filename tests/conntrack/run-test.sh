7da1e403b73d8203f2eef13e3bc162c4f43efe5a
Florian Westphal <fw@strlen.de>
2026-05-09 23:56:15 +0200

    tests: run-tests.sh: remove obsolete modprobe calls
    Not needed anymore, all modules were merged into nf_conntrack.ko.
    
    Signed-off-by: Florian Westphal <fw@strlen.de>

--- tests/conntrack/run-test.sh
+++ tests/conntrack/run-test.sh
@@ -7,15 +7,5 @@ then
 fi
 
 make test
-#
-# XXX: module auto-load not support by nfnetlink_cttimeout yet :-(
-#
-# any or all of these might be built-ins rather than modules, so don't error
-# out on failure from modprobe
-modprobe nf_conntrack_ipv4 || true
-modprobe nf_conntrack_ipv6 || true
-modprobe nf_conntrack_proto_udplite || true
-modprobe nf_conntrack_proto_sctp || true
-modprobe nf_conntrack_proto_dccp || true
-modprobe nf_conntrack_proto_gre || true
+
 ./test testcases
